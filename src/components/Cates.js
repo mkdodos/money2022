8d7edb1f93509219808968e38675ce411038aa5d
mk <mkdodos@gmail.com>
2022-08-17 17:51:57 +0800

    crud

--- src/components/Cates.js
+++ src/components/Cates.js
@@ -7,11 +7,14 @@ export default function Cates() {
     amt: '',
     income: '',
   });
+
+  const [editedIndex, setEditedIndex] = useState(-1);
+  const [rows, setRows] = useState([]);
   const schema = [
-    { name: 'date', text: '日期' },
-    { name: 'title', text: '項目' },
-    { name: 'amt', text: '金額' },
-    { name: 'income', text: '收入' },
+    { name: 'date', text: '日期', type: 'date' },
+    { name: 'title', text: '項目', type: 'text' },
+    { name: 'amt', text: '金額', type: 'number' },
+    { name: 'income', text: '收入', type: 'number' },
   ];
 
   let updateInput = (e) => {
@@ -21,18 +24,94 @@ export default function Cates() {
   return (
     <>
       <pre>{JSON.stringify(row)}</pre>
+      {/* {rows.map((row, i) => {
+        return <pre key={i}>{JSON.stringify(row)}</pre>;
+      })} */}
+      <pre>{editedIndex}</pre>
 
-      {schema.map((obj, i) => {
-        return (
-          <input
-            key={i}
-            name={obj.name}
-            onChange={updateInput}
-            value={row[obj.name]}
-            placeholder={obj.text}
-          />
-        );
-      })}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        {/* 表單輸入項 */}
+        {schema.map((obj, i) => {
+          return (
+            <input
+              key={i}
+              name={obj.name}
+              onChange={updateInput}
+              value={row[obj.name]}
+              placeholder={obj.text}
+              type={obj.type}
+            />
+          );
+        })}
+        <button
+          onClick={() => {
+            // this.editItem = Object.assign({}, item);
+            const editedItem= Object.assign({},row);
+            // const newRow = row.slice()
+            editedItem.id= Date.now()
+            // setRow(editedItem)
+            setRows([...rows, editedItem]);
+          }}
+        >
+          Add
+        </button>
+        <button
+          onClick={() => {
+            // 複製一份原資料
+            const newRows = rows.slice();
+            Object.assign(newRows[editedIndex], row);
+            // Object.assign(rows[editedIndex], row);
+            setRows(newRows)
+          }}
+        >
+          Update
+        </button>
+      </form>
+      {/* 表格資料 */}
+      <table>
+        <tbody>
+          {rows.map((row, i) => {
+            return (
+
+              <tr key={i}>
+               <td>{row.id}</td>
+                {schema.map((s, i) => {
+                  return <td key={i}>{row[s.name]}</td>;
+                })}
+                <td
+                  onClick={(e) => {
+                    // 點選列在 rows 中的索引
+                    setEditedIndex(rows.indexOf(row));
+                    // setRow({ ...row, row });
+                    setRow(row);
+                  }}
+                >
+                  Edit
+                </td>
+                <td
+                  onClick={(e) => {
+                    console.log(row.title)
+                    // 點選列在 rows 中的索引
+                    const newRows = rows.filter(obj=>{
+                      return obj.id!==row.id
+                    })
+                    // const newRows = rows.splice(editedIndex,1)
+                    // console.log(newRows)
+                    setRows(newRows)
+                    // setRow({ ...row, row });
+                  }}
+                >
+                  Delete
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
       {/* <input
         name="title"
         value={row[schema[0].name]}
