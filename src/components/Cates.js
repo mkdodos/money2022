b2209e1d34347943749c917f9e36e97b1cc615b1
mk <mkdodos@gmail.com>
2022-08-17 09:15:59 +0800

    input map

--- src/components/Cates.js
+++ src/components/Cates.js
@@ -1,264 +1,48 @@
-import { db, db_dada } from '../utils/firebase';
-import DataTable from './DataTable';
-import DataRow from './DataRow';
-import {
-  Button,
-  Form,
-  Table,
-  Divider,
-  Segment,
-  Input,
-  Grid,
-  Menu,
-  Dropdown,
-} from 'semantic-ui-react';
-import React from 'react';
-import { Cate } from './Cate';
-
+import { useState } from 'react';
 export default function Cates() {
-  let [state, setState] = React.useState({
-    docId: '',
-    contact: {
-      name: '',
-      prior: '',
-      groupId: '',
-    },
-    groups: [],
+  const [row, setRow] = useState({
+    // 每個都要有預設值,不然會報錯
+    date: '',
+    title: '',
+    amt: '',
+    income: '',
   });
-  const [rows, setRows] = React.useState([]);
-  // const [editedRow, setEditedRow] = React.useState({});
-  const [editIndex, setEditIndex] = React.useState(-1);
-  const [docId, setDocId] = React.useState('');
-  // const [name, setName] = React.useState('');
-  // const [prior, setPrior] = React.useState('');
   const schema = [
-    // { text: 'ID', value: 'id', type: 'string' },
-    { text: '名稱', value: 'name', type: 'string' },
-    { text: '順序', value: 'prior', type: 'number' },
-    // { text: '使用者', value: 'user', type: 'string' },
+    { name: 'date', text: '日期' },
+    { name: 'title', text: '項目' },
+    { name: 'amt', text: '金額' },
+    { name: 'income', text: '收入' },
   ];
 
-  const col_name = 'notes';
-  React.useEffect(() => {
-    db.collection(col_name)
-      .limit(30)
-      .get()
-      .then((snapshot) => {
-        const data = snapshot.docs.map((doc) => {
-          return { ...doc.data(), id: doc.id };
-        });
-        setRows(data);
-        const options = data.map((row) => {
-          return { text: row.name, value: row.id, key: row.id };
-        });
-        console.log(options);
-        setState({ ...state, groups: options });
-        // setState({ ...state, groups: data });
-      });
-  }, []);
-
-  // 刪除
-  const deleteTask = (id) => {
-    // 篩選資料排除點選的項目
-    const newTodoList = rows.filter((task) => {
-      return task.id !== id;
-    });
-
-    // firebase
-    db.collection(col_name).doc(id).delete();
-
-    // 清空欄位值
-    // setName('');
-    // setPrior('');
-    // 設定篩選後的資料
-    setRows(newTodoList);
-  };
-
-  // 編輯
-  const editTask = (item) => {
-    // console.log(item.name)
-    // 設定 editIndex 做為儲存時判斷新增或修改的依據
-    setEditIndex(rows.indexOf(item));
-    // 設定編輯列傳給子元件
-    // setEditedRow(item)
-    // 設定表單輸入元件的值
-    setState({
-      ...state,
-      contact: {
-        ...state.contact,
-        name: item.name,
-        prior: item.prior,
-        groupId: item.groupId,
-      },
-    });
-    // setName(item.name);
-    // setPrior(item.prior);
-    // 更新時會用到
-    setDocId(item.id);
-  };
-
-  // 儲存
-  const saveTask = () => {
-    // 複製一份原資料
-    const newRows = rows.slice();
-    // 欄位資料    
-    // const row = { name, prior };
-    // 修改
-    if (editIndex > -1) {
-      // 將欄位資料寫到複本
-      Object.assign(newRows[editIndex], state.contact);
-      // firebase
-      db.collection(col_name).doc(docId).update(state.contact);
-      // 清空欄位值
-      setEditIndex(-1);
-      setState({
-        ...state,
-        contact: { ...state.contact, name: '', prior: '' },
-      });
-      // setName('');
-      // setPrior('');
-    }
-    // 新增
-    else {
-      // const id = Date.now();
-      // 將欄位資料加到複本
-      // newRows.push({ ...row, id });
-
-      // firebase
-      db.collection(col_name)
-        .add(state.contact)
-        .then((doc) => {
-          // newRows.push({ ...row, id: doc.id });
-          newRows.push({ ...state.contact, id: doc.id });
-          // 將複本寫到原資料
-          setRows(newRows);
-        });
-      // 清空欄位值
-      setState({
-        ...state,
-        contact: { ...state.contact, name: '', prior: '' },
-      });
-      // setName('');
-      // setPrior('');
-    }
-  };
-  let { contact, groups } = state;
-
-  let updateInput = (event) => {
-   
-    setState({
-      ...state,
-      contact: {
-        ...state.contact,
-        [event.target.name]: event.target.value,
-      },
-    });
-   
-
-    
+  let updateInput = (e) => {
+    setRow({ ...row, [e.target.name]: e.target.value });
   };
 
   return (
     <>
-      <pre>{JSON.stringify(contact)}</pre>
-      {/* <pre>{JSON.stringify(groups[0])}</pre> */}
-      {/* <pre>{groups[0]}</pre> */}
-      <Grid>
-        <Grid.Row>
-          <Grid.Column>
-            <Form unstackable>
-              <Form.Group widths={2}>
-                <Form.Input
-                  fluid
-                  label="First name"
-                  name="name"
-                  value={contact.name}
-                  onChange={updateInput}
-                  // onChange={(e) => {
-                  //   setName(e.target.value);
-                  // }}
-                  placeholder="First name"
-                />
-                <Form.Input
-                  name="prior"
-                  value={contact.prior}
-                  type="number"
-                  onChange={updateInput}
-                  // onChange={(e) => {
-                  //   setPrior(e.target.value);
-                  // }}
-                  fluid
-                  label="金額"
-                  placeholder=""
-                />
+      <pre>{JSON.stringify(row)}</pre>
 
-                <Form.Select
-                  selection
-                  // text="Dropdown"
-                  name="groupId"
-                  value={contact.groupId}
-                  onChange={(e, obj) => {
-                    setState({
-                      ...state,
-                      contact: { ...state.contact, groupId: obj.value },
-                    });
-                  }}
-                  options={groups}
-                  placeholder="下拉"
-                />
-
-                {/* <select
-                  name="groupId"
-                  value={contact.groupId}
-                  onChange={updateInput}
-                >
-                  {groups.map((group) => {
-                    return (
-                      <option key={group.id} value={group.id}>
-                        {group.name}
-                      </option>
-                    );
-                  })}
-                </select> */}
-              </Form.Group>
-
-              <Button color="teal" floated="right" onClick={saveTask}>
-                儲存
-              </Button>
-            </Form>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            {' '}
-            <Table unstackable>
-              <Table.Header>
-                <Table.Row>
-                  {schema.map((obj, i) => (
-                    <Table.HeaderCell key={i}>{obj.text}</Table.HeaderCell>
-                  ))}
-                  <Table.HeaderCell>刪除</Table.HeaderCell>
-                  <Table.HeaderCell>編輯</Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                {rows.map((task, i) => {
-                  return (
-                    <Cate
-                      key={i}
-                      index={i}
-                      schema={schema}
-                      row={task}
-                      deleteTask={deleteTask}
-                      editTask={editTask}
-                    />
-                  );
-                })}
-              </Table.Body>
-            </Table>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      {schema.map((obj, i) => {
+        return (
+          <input
+            key={i}
+            name={obj.name}
+            onChange={updateInput}
+            value={row[obj.name]}
+            placeholder={obj.text}
+          />
+        );
+      })}
+      {/* <input
+        name="title"
+        value={row[schema[0].name]}
+        onChange={updateInput}       
+      /> */}
+      {/* <input
+        name="amt"
+        value={row[schema[1].name]}
+        onChange={updateInput}       
+      /> */}
     </>
   );
 }
