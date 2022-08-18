1eb0ae57c251f78f2df3dbf5b395bd589616f2a6
mk <mkdodos@gmail.com>
2022-08-18 11:37:57 +0800

    ui

---
+++ src/components/Cates copy 3.js
@@ -0,0 +1,127 @@
+import { useState } from 'react';
+export default function Cates() {
+  const [row, setRow] = useState({
+    // 每個都要有預設值,不然會報錯
+    date: '',
+    title: '',
+    amt: '',
+    income: '',
+  });
+
+  const [editedIndex, setEditedIndex] = useState(-1);
+  const [rows, setRows] = useState([]);
+  const schema = [
+    { name: 'date', text: '日期', type: 'date' },
+    { name: 'title', text: '項目', type: 'text' },
+    { name: 'amt', text: '金額', type: 'number' },
+    { name: 'income', text: '收入', type: 'number' },
+  ];
+
+  let updateInput = (e) => {
+    setRow({ ...row, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <>
+      <pre>{JSON.stringify(row)}</pre>
+      {/* {rows.map((row, i) => {
+        return <pre key={i}>{JSON.stringify(row)}</pre>;
+      })} */}
+      <pre>{editedIndex}</pre>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        {/* 表單輸入項 */}
+        {schema.map((obj, i) => {
+          return (
+            <input
+              key={i}
+              name={obj.name}
+              onChange={updateInput}
+              value={row[obj.name]}
+              placeholder={obj.text}
+              type={obj.type}
+            />
+          );
+        })}
+        <div class="ui input"><input type="text" placeholder="Search..."/></div>
+        {/* <button class="ui button">Click Here</button> */}
+        <button class="ui button"
+          onClick={() => {
+            // const editedItem = Object.assign({}, row);
+            // editedItem.id = Date.now();
+            // setRows([...rows, editedItem]);
+            // 在原 rows 加上包含 id 的 row
+            setRows([...rows, { ...row, id: Date.now() }]);
+          }}
+        >
+          Add
+        </button>
+        <button class="ui button primary "
+          onClick={() => {
+            // 複製一份原資料
+            const newRows = rows.slice();
+            Object.assign(newRows[editedIndex], row);
+            // Object.assign(rows[editedIndex], row);
+            setRows(newRows);
+          }}
+        >
+          Update
+        </button>
+      </form>
+      {/* 表格資料 */}
+      <table>
+        <tbody>
+          {rows.map((row, i) => {
+            return (
+              <tr key={i}>
+                <td>{row.id}</td>
+                {schema.map((s, i) => {
+                  return <td key={i}>{row[s.name]}</td>;
+                })}
+                <td
+                  onClick={(e) => {
+                    // 點選列在 rows 中的索引
+                    setEditedIndex(rows.indexOf(row));
+                    // setRow({ ...row, row });
+                    setRow(row);
+                  }}
+                >
+                  Edit
+                </td>
+                <td
+                  onClick={(e) => {
+                    console.log(row.title);
+                    // 點選列在 rows 中的索引
+                    const newRows = rows.filter((obj) => {
+                      return obj.id !== row.id;
+                    });
+                    // const newRows = rows.splice(editedIndex,1)
+                    // console.log(newRows)
+                    setRows(newRows);
+                    // setRow({ ...row, row });
+                  }}
+                >
+                  Delete
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {/* <input
+        name="title"
+        value={row[schema[0].name]}
+        onChange={updateInput}       
+      /> */}
+      {/* <input
+        name="amt"
+        value={row[schema[1].name]}
+        onChange={updateInput}       
+      /> */}
+    </>
+  );
+}
--- src/components/Cates.js
+++ src/components/Cates.js
@@ -1,127 +1,198 @@
-import { useState } from 'react';
-export default function Cates() {
-  const [row, setRow] = useState({
-    // 每個都要有預設值,不然會報錯
-    date: '',
-    title: '',
-    amt: '',
-    income: '',
-  });
+// 目標: 建立資料結構,自動產生表單和表格標題列
+// 建立資料,自動產生表格內容
 
-  const [editedIndex, setEditedIndex] = useState(-1);
-  const [rows, setRows] = useState([]);
-  const schema = [
-    { name: 'date', text: '日期', type: 'date' },
-    { name: 'title', text: '項目', type: 'text' },
-    { name: 'amt', text: '金額', type: 'number' },
-    { name: 'income', text: '收入', type: 'number' },
-  ];
+import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import { Button, Table } from 'semantic-ui-react';
 
-  let updateInput = (e) => {
-    setRow({ ...row, [e.target.name]: e.target.value });
-  };
+// 產生表單
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <>
-      <pre>{JSON.stringify(row)}</pre>
-      {/* {rows.map((row, i) => {
-        return <pre key={i}>{JSON.stringify(row)}</pre>;
-      })} */}
-      <pre>{editedIndex}</pre>
-
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-      >
-        {/* 表單輸入項 */}
-        {schema.map((obj, i) => {
+  render() {
+    return (
+      <>
+        {/* {JSON.stringify(this.props.schema)} */}
+        {this.props.schema.map((obj, i) => {
           return (
-            <input
-              key={i}
-              name={obj.name}
-              onChange={updateInput}
-              value={row[obj.name]}
-              placeholder={obj.text}
-              type={obj.type}
-            />
+            <div className="ui input" key={i}>
+              <input onChange={() => {}} placeholder={obj.text} />
+            </div>
           );
         })}
-        <button
+        <button className="ui button" onClick={this.props.onClick}>
+          新增
+        </button>
+        {/* <button
+          className="ui button"
           onClick={() => {
-            // this.editItem = Object.assign({}, item);
-            const editedItem= Object.assign({},row);
-            // const newRow = row.slice()
-            editedItem.id= Date.now()
-            // setRow(editedItem)
-            setRows([...rows, editedItem]);
+            this.props.onClick();
           }}
         >
-          Add
-        </button>
-        <button
-          onClick={() => {
-            // 複製一份原資料
-            const newRows = rows.slice();
-            Object.assign(newRows[editedIndex], row);
-            // Object.assign(rows[editedIndex], row);
-            setRows(newRows)
-          }}
-        >
-          Update
-        </button>
-      </form>
-      {/* 表格資料 */}
-      <table>
-        <tbody>
-          {rows.map((row, i) => {
-            return (
+          新增
+        </button> */}
+      </>
+    );
+  }
+}
 
-              <tr key={i}>
-               <td>{row.id}</td>
-                {schema.map((s, i) => {
-                  return <td key={i}>{row[s.name]}</td>;
-                })}
-                <td
-                  onClick={(e) => {
-                    // 點選列在 rows 中的索引
-                    setEditedIndex(rows.indexOf(row));
-                    // setRow({ ...row, row });
-                    setRow(row);
-                  }}
-                >
-                  Edit
-                </td>
-                <td
-                  onClick={(e) => {
-                    console.log(row.title)
-                    // 點選列在 rows 中的索引
-                    const newRows = rows.filter(obj=>{
-                      return obj.id!==row.id
-                    })
-                    // const newRows = rows.splice(editedIndex,1)
-                    // console.log(newRows)
-                    setRows(newRows)
-                    // setRow({ ...row, row });
-                  }}
-                >
-                  Delete
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      {/* <input
-        name="title"
-        value={row[schema[0].name]}
-        onChange={updateInput}       
+// 產生表格
+class MyTableOLD extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <table className="ui unstackable table">
+          <thead>
+            <tr>
+              {this.props.schema.map((header, i) => {
+                return <th key={i}>{header.name}</th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.rows.map((obj, i) => {
+              return (
+                <tr key={i}>
+                  <td>{obj.date}</td>
+                  <td>{obj.title}</td>
+                  <td>{obj.expense}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+class MyTable extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <Table unstackable>
+          <Table.Header>
+            <Table.Row>
+              {this.props.schema.map((header, i) => {
+                return (
+                  <Table.HeaderCell key={i}>{header.text}</Table.HeaderCell>
+                );
+              })}
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {this.props.rows.map((obj, i) => {
+              return (
+                <tr key={i}>
+                  <td>{obj.date}</td>
+                  <td>{obj.title}</td>
+                  <td>{obj.expense}</td>
+                </tr>
+              );
+            })}
+          </Table.Body>
+        </Table>
+      </>
+    );
+  }
+}
+
+export default function Cates() {
+  const schema = [
+    // {
+    //   name: '欄位名稱',
+    //   text: '表格標題列欄位文字, 表單 placeholder 文字',
+    //   type: '表單輸入項類型',
+    // },
+    {
+      name: 'date',
+      text: '日期',
+      type: 'date',
+    },
+    {
+      name: 'title',
+      text: '項目',
+      type: 'text',
+    },
+    {
+      name: 'expense',
+      text: '支出',
+      type: 'number',
+    },
+  ];
+
+  const data = [
+    { date: '2022-08-17', title: '蛋餅', expense: '30' },
+    { date: '2022-08-18', title: '土司', expense: '20' },
+  ];
+
+  const [itemList, setItemList] = React.useState([]);
+  const [item, setItem] = React.useState({
+    date: '',
+    title: '',
+    expense: '',
+  });
+
+  React.useEffect(() => {
+    setItemList(data);
+  }, []);
+
+  function handleClick() {
+    // setItem({ date: '2022-08-17', title: '蛋餅', expense: '30' });
+    // console.log(item);
+    // const data = itemList
+    setItemList([
+      ...itemList,
+      // 將這筆資料和表單結合
+      // 取得表單的值
+      item,
+    ]);
+  }
+  return (
+    <>
+      <pre>{JSON.stringify(item)}</pre>
+      {schema.map((obj, i) => {
+        return (
+          <div className="ui input" key={i}>
+            <input
+              name={obj.name}
+              value={item[obj.name]}
+              type={obj.type}
+              onChange={(e) => {
+                setItem({ ...item, [e.target.name]: e.target.value });
+              }}
+              placeholder={obj.text}
+            />
+          </div>
+        );
+      })}
+      <button className="ui button" onClick={handleClick}>
+        新增
+      </button>
+
+      {/* <MyForm
+        schema={schema}
+        onClick={() => {
+          // data.push({ date: '2022-08-17', title: '蛋餅', expense: '30' })
+          setItemList([
+            ...itemList,
+            // 將這筆資料和表單結合
+            // 取得表單的值
+            { date: '2022-08-17', title: '蛋餅', expense: '30' },
+          ]);
+        }}
       /> */}
-      {/* <input
-        name="amt"
-        value={row[schema[1].name]}
-        onChange={updateInput}       
-      /> */}
+      <MyTable schema={schema} rows={itemList} />
     </>
   );
 }
