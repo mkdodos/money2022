a03291ccee246136ffc558cfb5dd80d2a18fbc29
mk <mkdodos@gmail.com>
2022-11-24 17:21:34 +0800

    hhh

--- src/pages/Food/component/EditForm.js
+++ src/pages/Food/component/EditForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   Input,
@@ -16,26 +16,93 @@ import {
 import image from '../../../img/image.png';
 
 export default function EditForm() {
+  const [price, setPrice] = useState(140);
+  const [qty, setQty] = useState(0);
+  const add = () => {
+    setQty((prev)=>{
+      return prev+1;
+    })
+  };
+
+  const minus = () => {
+    setQty((prev)=>{
+      return prev-1;
+    })
+  };
   return (
     <div>
-      
+      <Segment.Group horizontal>
+        <Segment>
+          <Image src={image} size="small" color="pink" />
+          <Header as="h3"> 美式單層牛肉堡 </Header>
+          <Header as="h4"> ${price}</Header>
+          <Divider />
+          {/* <Header as="h4"> 小計 ${price*qty}</Header> */}
+          {/* <Divider /> */}
+          <Button circular icon="minus" size='small' onClick={minus} />
+          <Button basic>{qty}</Button>
+          <Button circular icon="plus" size='small' color="pink" onClick={add} />
+          <Button  color="pink" floated='right' >加入購物車</Button>
+        </Segment>
 
-      <Image src={image} size="small" color="pink" />
-     <Divider/>
-      {/* <Menu widths={3}>
-      
+        {/* <Segment>
+          <Image src={image} size="small" color="pink" />
+          <Header as="h3"> 美式單層牛肉堡 </Header>
+          <Header as="h4"> ${price}</Header>
+          <Header as="h4"> 小計 ${price*qty}</Header>
+          <Divider />
+          <Button circular icon="minus" size='small' onClick={minus} />
+          <Button basic>{qty}</Button>
+          <Button circular icon="plus" size='small' color="pink" onClick={add} />
+        </Segment> */}
 
-        <Menu.Item>
-          <Label circular>-</Label>
-        </Menu.Item>
+        {/* <Segment>
+          <Image src={image} size="small" color="pink" />
+          <Header as="h4"> 起司牛肉堡 </Header>
+          <Header as="h4"> $160</Header>
+          <Divider />
+          <Button circular icon="minus" />
+          <Button basic>0</Button>
+          <Button circular icon="plus" color="pink" />
+        </Segment> */}
+      </Segment.Group>
 
-        <Menu.Item name="testimonials">0</Menu.Item>
-        <Menu.Item name="features">
-          <Label circular color="pink">
-            +
-          </Label>
-        </Menu.Item>
-      </Menu> */}
+      <Segment.Group horizontal>
+        <Segment>
+          <Image rounded src={image} size="small" color="pink" />
+          <Header as="h4"> 法式鮮蔬湯品 </Header>
+          <Header as="h4"> $40</Header>
+          {/* <Divider /> */}
+          {/* <Button circular icon="minus" />
+          <Button basic>0</Button>
+          <Button circular icon="plus" color="pink" /> */}
+        </Segment>
+
+        <Segment>
+          <Image rounded src={image} size="small" color="pink" />
+          <Header as="h4"> 鮮蝦輕沙拉 </Header>
+          <Header as="h4"> $140</Header>
+          {/* <Divider /> */}
+          {/* <Button circular icon="minus" />
+          <Button basic>0</Button>
+          <Button circular icon="plus" color="pink" /> */}
+        </Segment>
+      </Segment.Group>
+
+      {/* <Segment>
+        {' '}
+        <Image src={image} size="small" color="pink" />
+        <Header> 美式牛肉堡 $140</Header>
+        <Divider />
+        <Button circular icon="minus" />
+        <Button basic>0</Button>
+        <Button circular icon="plus" color="pink" />
+      </Segment>
+      <Segment> </Segment> */}
+
+      {/* <Image src={image} size="small" color="pink" />
+      <Header> 美式牛肉堡 $140</Header>
+      <Divider /> */}
 
       {/* <Label circular size="large">
         -
@@ -44,11 +111,6 @@ export default function EditForm() {
       <Label circular size="large" color="pink">
         <Icon name="plus" />
       </Label> */}
-
-<Button circular icon="minus"  />
-<Button basic>0</Button>
-<Button circular icon="plus" color="pink" />
-
 
       {/* <Button color="pink" circular>+</Button> */}
       {/* <Grid>
