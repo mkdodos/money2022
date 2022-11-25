16a3b2038f2350624379a4412d99ba8466c36c39
mk <mkdodos@gmail.com>
2022-11-25 10:38:27 +0800

    single

--- src/pages/Food/component/ProdList.js
+++ src/pages/Food/component/ProdList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import image from '../../../img/image.png';
 
+import { v4 as uuidv4 } from 'uuid';
+
 import { Segment, Image, Header } from 'semantic-ui-react';
 export default function ProdList() {
   const rows = [
@@ -29,17 +31,17 @@ export default function ProdList() {
       name: '牛肉堡5',
       price: 50,
     },
-    {
-      id: '6',
-      name: '豬肉堡6',
-      price: 60,
-    },
+    // {
+    //   id: '6',
+    //   name: '豬肉堡6',
+    //   price: 60,
+    // },
   ];
   return (
     <div>
       {rows.map((row, index) => {
         return (
-          <>
+          <div key={row.id}>
             {index % 2 == 0 && (
               <Segment.Group horizontal>
                 <Segment>
@@ -48,14 +50,17 @@ export default function ProdList() {
                   <Header as="h4"> ${row.price}</Header>
                 </Segment>
 
-                <Segment>
-                  <Image rounded src={image} size="small" color="pink" />
-                  <Header as="h4"> {rows[index+1].name} </Header>
-                  <Header as="h4"> ${rows[index+1].price}</Header>
-                </Segment>
+                {/* 判斷索引未達最大值才顯示 */}
+                {index < rows.length - 1 && (
+                  <Segment>
+                    <Image rounded src={image} size="small" color="pink" />
+                    <Header as="h4"> {rows[index + 1].name} </Header>
+                    <Header as="h4"> ${rows[index + 1].price}</Header>
+                  </Segment>
+                )}
               </Segment.Group>
             )}
-          </>
+          </div>
         );
       })}
     </div>
